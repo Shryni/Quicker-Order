ed4651442de69565832e8464113eeff911678bec
shrynikedambadi <kedambadishryni@gmail.com>
2019-10-30 13:54:40 +0000

    first commit

---
+++ src/main/java/com/nci/cad/quickerorder/QuickerOrderApplication.java
@@ -0,0 +1,13 @@
+package com.nci.cad.quickerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickerOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickerOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nci/cad/quickerorder/QuickerOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nci.cad.quickerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickerOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
