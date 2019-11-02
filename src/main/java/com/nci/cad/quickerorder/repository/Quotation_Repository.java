e01f100315d2a09f091ab7892edea02896ce5ba0
Pranav <pranavessentials@gmail.com>
2019-11-02 10:31:02 +0000

    -changed repository classes parameters(Invoice,PurchaseOrder,Quotation,VendorStore)

--- src/main/java/com/nci/cad/quickerorder/repository/Quotation_Repository.java
+++ src/main/java/com/nci/cad/quickerorder/repository/Quotation_Repository.java
@@ -1,9 +1,10 @@
 package com.nci.cad.quickerorder.repository;
 
+import com.nci.cad.quickerorder.model.Quotation;
 import com.nci.cad.quickerorder.model.Requestor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Quotation_Repository extends JpaRepository<Requestor , Long> {
+public interface Quotation_Repository extends JpaRepository<Quotation, Long> {
 }
--- src/main/java/com/nci/cad/quickerorder/repository/VendorStore_Repository.java
+++ src/main/java/com/nci/cad/quickerorder/repository/VendorStore_Repository.java
@@ -2,10 +2,11 @@ package com.nci.cad.quickerorder.repository;
 
 
 import com.nci.cad.quickerorder.model.RequestorStore;
+import com.nci.cad.quickerorder.model.VendorStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VendorStore_Repository extends JpaRepository<RequestorStore,Long> {
+public interface VendorStore_Repository extends JpaRepository<VendorStore,Long> {
     // Group findByName(String name);
 }
