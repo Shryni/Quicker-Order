db3afbe6870efef24f1dd38e535080a7c161737b
Pranav <pranavessentials@gmail.com>
2019-11-07 21:51:52 +0000

    Merge branches 'feature-pranav' and 'feature-shryni' of https://github.com/Shryni/Quicker-Order into feature-pranav
     Conflicts:
    	build.gradle
    	src/main/java/com/nci/cad/quickerorder/controller/InvoiceController.java

--- src/main/java/com/nci/cad/quickerorder/controller/ViewController.java
+++ src/main/java/com/nci/cad/quickerorder/controller/ViewController.java
@@ -39,14 +39,14 @@ public class ViewController {
 
     @GetMapping("/spendings/{id}")
     public String invoiceChart(@Valid @PathVariable long id, Model model){
-        Map<Date , Float> invoiceMap = requestorStore_service.getInvoiceDetails(id);
-        model.addAttribute("invoiceMap", invoiceMap);
-//        Map<Date , Float> invoiceMap = new LinkedHashMap<>();
-//        invoiceMap.put(Date.valueOf("2019-12-12"),(float) 450.0);
-//        invoiceMap.put(Date.valueOf("2018-01-12"),(float) 3560.70);
-//        invoiceMap.put(Date.valueOf("2019-02-23"),(float) 1480.4);
-//        invoiceMap.put(Date.valueOf("2019-05-19"),(float) 4689.120);
-//        model.addAttribute("invoiceMap",invoiceMap);
+        //Map<Date , Float> invoiceMap = requestorStore_service.getInvoiceDetails(id);
+        //model.addAttribute("invoiceMap", invoiceMap);
+        Map<Date , Float> invoiceMap = new LinkedHashMap<>();
+        invoiceMap.put(Date.valueOf("2019-12-12"),(float) 450.0);
+        invoiceMap.put(Date.valueOf("2018-01-12"),(float) 3560.70);
+        invoiceMap.put(Date.valueOf("2019-02-23"),(float) 1480.4);
+        invoiceMap.put(Date.valueOf("2019-05-19"),(float) 4689.120);
+        model.addAttribute("invoiceMap",invoiceMap);
         return "invoiceGraph";
     }
 
